00dde895fd2d1983bd41200118b1ed495c6a4831
Twan Pluymaekers <twanpluy@gmail.com>
2022-11-23 17:37:59 +0100

    Create serialer, model change app register

--- synth_backend/apps/synths/models.py
+++ synth_backend/apps/synths/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Synths(models.Model):
+    name = models.CharField(max_length=100)
+    brand = models.CharField(max_length=100)
+    type = models.CharField(max_length=100, blank=True)
+    mode = models.CharField(max_length=100, blank=True)
+    image = models.CharField(max_length=1000)
+    description = models.CharField(max_length=1000)
+    url = models.CharField(max_length=1000, blank=True)
+    create_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='synths')
+    create_at = models.DateTimeField(auto_now_add=True)
+    modified_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='+')
+    modified_at = models.DateTimeField(auto_now=True)
+    
+    
+    def __str__(self):
+        return self.name
