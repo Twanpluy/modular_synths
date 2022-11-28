d543520acbf8c9f131d0cd51fa61f7e5717d6d1a
Twan Pluymaekers <twanpluy@gmail.com>
2022-11-28 20:42:15 +0100

    Added Toast pop up, delete functions

--- synth_vue/src/views/synths/synthList.vue
+++ synth_vue/src/views/synths/synthList.vue
@@ -1,17 +1,50 @@
 <template>
-    <div class=""
-         v-for="synth in synths"
-         v-bind:key="synth.id"
-    >
-        <a class="">{{synth.name}}</a>    
-    </div>    
- 
+<div class="synth-list">
 
+  <button class="button bg-green-600 rounded-3xl" @click="addSynth()">Add Synth</button>
 
+  <div class="overflow-x-auto relative shadow-md sm:rounded-lg">
+    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+                <th scope="col" class="py-3 px-6">
+                    Name
+                </th>
+                <th scope="col" class="py-3 px-6">
+                    Description
+                </th>
+                <th scope="col" class="py-3 px-6">
+                    Type
+                </th>
+                <th scope="col" class="py-3 px-6">
+                  <span class="sr-only">
+                    Edit</span>
+                </th>
+                <th scope="col" class="py-3 px-6">
+                  Delete
+                </th>
+            </tr>
+        </thead>
+  <tbody>
+    <tr v-for="synth in synths" :key="synth.id">
+      <td>{{synth.name}}</td>
+      <td>{{synth.description}}</td>
+      <td>{{synth.type}}</td>
+      <td><router-link :to="'/synth/' + synth.id + '/edit'">Edit</router-link></td>
+      <td><button class="bg-red-700 round text-slate-400 hover:text-blue-600" @click="deleteSynth(synth.id)">Delete</button></td>
+    </tr>
+
+  </tbody>
+</table>
+</div>
+</div>
 </template>
 
 <script>
 import axios from 'axios';
+import { useToast } from "vue-toastification";
+import "vue-toastification/dist/index.css";
+
 
 export default {
   name: 'synthList',
@@ -19,6 +52,12 @@ export default {
     return {
       synths: [],
     };
+  },
+  setup() {
+    const toast = useToast();
+          // or with options
+      return { toast }
+
   },
   mounted() {
     this.getSynths();
@@ -34,13 +73,31 @@ export default {
         console.log(JSON.stringify(error))
     } )
   },
-  addSynths(){
-    axios.post('/api/v1/synths/')
-    .then
-
+    addSynth(){
+    axios.post('/api/v1/synths/', {
+        name: "test",
+        description: "test",
+        price: 0,
+        image: "test",
+        owner: 1
+    }).then(response => {
+        this.synths.push(response.data)
+        this.toast.success("Synth added")
+    }).catch(error => {
+      this.toast.error("Synth not added")
+        console.log(JSON.stringify(error))
+    } )
+    },
+    deleteSynth(id){
+    axios.delete('/api/v1/synths/' + id + '/')
+    .then(response => {
+        this.synths = this.synths.filter(synth => synth.id !== id)
+        this.toast.success("Synth deleted")
+    }).catch(error => {
+      this.toast.error("Synth not deleted")
+        console.log(JSON.stringify(error))
+    } )
+    }
+  },
   }
-}
-
-}
-
 </script>
