1b7c6e7e99a93bd0344c9d6e13afba85693edd00
Twan Pluymaekers <twanpluy@gmail.com>
2022-11-30 15:36:36 +0100

    Add ebay model to admin

--- synth_backend/apps/synths/admin.py
+++ synth_backend/apps/synths/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from .models import Synths
+from .models import Synths,Ebay_Synths
 # Register your models here.
 admin.site.register(Synths)
+admin.site.register(Ebay_Synths)
