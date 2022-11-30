911ac4fed1f4fa1025e5fa84b7b1faf7c960f29e
Twan Pluymaekers <twanpluy@gmail.com>
2022-11-30 13:13:15 +0100

    changed model with file path and name

--- synth_backend/apps/synths/models.py
+++ synth_backend/apps/synths/models.py
@@ -14,7 +14,8 @@ class Synths(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     modified_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='+')
     modified_at = models.DateTimeField(auto_now=True)
-    
+    file_name = models.CharField(max_length=1000, blank=True)
+    file_path = models.CharField(max_length=1000, blank=True)    
     
     def __str__(self):
-        return self.name
+        return self.name , self.brand
